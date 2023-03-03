e0a927c10027b343f4e50baf2e84d77be8b30c31
Kathy <kath.scholle@gmail.com>
2023-03-02 21:48:35 -0500

    initialize firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB38Zhr2JhYQIVERAXnili2icqBMoPWnPU",
+  authDomain: "investing-buddy-app.firebaseapp.com",
+  projectId: "investing-buddy-app",
+  storageBucket: "investing-buddy-app.appspot.com",
+  messagingSenderId: "181490635572",
+  appId: "1:181490635572:web:13ed50793a9dbbec980b61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
