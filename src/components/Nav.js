44316609d23f98d3948cd9d801542e6a25b8fc18
Kathy <kath.scholle@gmail.com>
2023-03-04 18:12:20 -0500

    adds firebase part 1 & css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,29 @@
 import {Link } from 'react-router-dom';
+import {login, logout} from '../firebase';
 
 const Nav = (props) => {
     return (
-        <nav>
+        <nav className="nav">
             <Link to='/stocks'>
                 <div>Stock App</div>
             </Link>
+        <ul>
+        {props.user ? 
+        <>
+        <li>Welcome, {props.user.displayName}</li>
+        <li> 
+          <img src={props.user.photoURL} alt={props.user.displayName} />
+        </li>
+        <li>
+          <button onClick={logout}>Logout</button>
+        </li>
+        </>
+        :
+        <li>
+          <button onClick={login}>Login</button>
+        </li>
+        }
+      </ul>
         </nav>
     );
 };
