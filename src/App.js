27cd0f9745ec6c034c27c4cc1eb72353a93e7750
Kathy <kath.scholle@gmail.com>
2023-03-04 18:12:20 -0500

    adds firebase part 1 & css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
+import {auth} from './firebase';
+import {useState, useEffect} from 'react';
 import Main from './components/Main';
 import Nav from './components/Nav';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(()=>{
+    auth.onAuthStateChanged(user=>setUser(user))
+  },[]);
+
   return (
     <div className="App">
-        <Nav />
+        <Nav user={user}/>
         <Main />
     </div>
   );
