396732f5c67f8d0a58b291fd2811addb1e224739
Daniel Fields <dgfields4193@gmail.com>
2023-03-08 11:30:03 -0500

    wip: add format to percent and market cap on show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,6 +5,7 @@ const Show = (props) => {
     const { id } = useParams();
     const stocks = props.stocks;
     const stock = stocks ? stocks.find((s) => s._id === id) : null;
+    const percentChange = stock.changesPercentage.toLocaleString(undefined, { maximumFractionDigits: 3});
     const [newForm, setCommentForm] = useState({
         comments: ''
     });
@@ -23,16 +24,15 @@ const Show = (props) => {
             comments: ''
         });
     };
- 
+
     const loadedStocks = () => {
         return(
             <>
                 <h1>{stock.name} ({stock.symbol})</h1>
-                <p>${stock.price}</p>
-                <p>{stock.changesPercentage}%</p>
-                <p>Market Cap: {stock.marketCap}</p>
+                <p>Current Price: ${stock.price} ( {percentChange}% )</p>
+                <p>Market Cap: {stock.marketCap.toLocaleString()}</p>
                 <p>EPS: {stock.eps}</p>
-                <p>PE: {stock.pe}</p>
+                <p>PE: {stock.pe === null ? 'Not Available' : stock.pe}</p>
             </>
         );
     };
