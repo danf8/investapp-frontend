36af1912c326951e32104d8b59fb243b437195f3
Daniel Fields <dgfields4193@gmail.com>
2023-03-14 15:41:08 -0400

    wip: renders chart of s&p on homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,92 @@
+import {useState, useEffect} from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
 
 const Homepage = (props) => {
+    const [stockIndexState, setStockIndex ] = useState(null)
+    const API_INDEX_URL = "http://localhost:3002/";
+
+    const getStockIndex = async () => {
+        try {
+                const response = await fetch(API_INDEX_URL, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'Application/json',
+                    }
+                });
+                const data = await response.json();
+                setStockIndex(data);    
+        } catch (error) {
+            console.error(error);
+        };
+    };
+
+//loads chart 
+  const loadChart = () => {
+    const stockClose = stockIndexState[0].historical.map((stock) => stock.close)
+    const labels = stockIndexState[0].historical.map((stock) => stock.date) 
+    //reverse stockindexstate to display information in correct over on chart
+    stockIndexState[0].historical.reverse()
+
+  const options = {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top' 
+        },
+        title: {
+          display: true,
+          text: 'SPY Chart',
+        },
+      },
+    };
+    //set chart data
+  const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'SPY Historical Prices',
+          data: stockClose,
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+      ],
+    };
+          return <Line options={options} data={data} />;
+      };
+
+    const loadingChart = () => {
+        return <h1>Loading chart.....</h1>;
+    };
+
+    useEffect(() => {
+      getStockIndex();
+  },[]);
+
     return(
-        <h1>Please login</h1>
+        <div>
+            <h1>Please login</h1>
+            {stockIndexState ?  loadChart(): loadingChart()}
+        </div>
     );
 };
 
