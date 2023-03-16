e27fe2cd7ed2046741a49512ea833b95967c2395
Kathy <kath.scholle@gmail.com>
2023-03-15 20:01:49 -0400

    changed api url to prod

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ import Homepage from '../pages/Homepage';
 const Main = (props) => {
         const [stocks, setStocks] = useState(null);
         // const API_URL = "http://localhost:5000/stocks";
-        const API_URL = "https://git.heroku.com/investing-buddy.git/stocks";
+        const API_URL = "https://investing-buddy.herokuapp.com/stocks";
 
         const getStocks = useCallback(async () => {
             try {
