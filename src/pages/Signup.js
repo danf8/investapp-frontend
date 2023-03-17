017be2abe6d83f59e684896f2d934e37bd2dcffa
Daniel Fields <dgfields4193@gmail.com>
2023-03-17 12:17:12 -0400

    wip: import css&  edit form element

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { signUp, profileUpdate} from '../firebase';
+import '../Css/signup.css'
 
 function SignUp(props){
     const [email, setEmail] = useState("");
@@ -35,22 +36,23 @@ function SignUp(props){
     };
 
     return (
-    <>
-    <h1>Sign Up Page</h1>
-    <form onSubmit={handleSignup}>
-        <label>
-             Display Name: <input type="text" name="displayName" value={displayName} onChange={handleNameChange} required/>
-        </label><br/><br/>
-        <label>
-             Email: <input type="email" name="email" value={email} onChange={handleEmailChange} required/>
-        </label><br/><br/>
-        <label>
-            Password: <input type="password" name="password" value={password} onChange={handlePasswordChange} required/>
-        </label><br/><br/>
-        <Link to="/signin">Sign in instead</Link>
-        <input id="SignupBtn" type="submit" value="Sign Up"/>
-    </form>
-    </>
+      <div className='signup-form'>
+        <h1>Sign Up Page</h1>
+        <form onSubmit={handleSignup}>
+            <label>
+                Display Name: <input type="text" name="displayName" value={displayName} onChange={handleNameChange} required/>
+            </label><br/><br/>
+            <label>
+                Email: <input type="email" name="email" value={email} onChange={handleEmailChange} required/>
+            </label><br/><br/>
+            <label>
+                Password: <input type="password" name="password" value={password} onChange={handlePasswordChange} required/>
+            </label><br/><br/>
+            <Link to="/signin">Sign in instead</Link>
+            <input id="SignupBtn" type="submit" value="Sign Up"/>
+        </form>
+        <small>Password must be longer than 6</small>
+      </div>
     )
 };
 
