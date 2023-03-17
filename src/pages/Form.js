5bca32a44317563e675fb92986d42c5f5d0f4705
Kathy <kath.scholle@gmail.com>
2023-03-16 22:14:01 -0400

    adds Form.js

---
+++ src/pages/Form.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+
+const Form = (props) => {
+    const [newUser, setNewUser] = useState(false);
+    const [notNewUser, setNotNewUser] = useState(false);
+
+    const handleNewUserChange = (event) => {
+        setNewUser(event.target.checked);
+      };
+    
+    const handleNotNewUserChange = (event) => {
+        setNotNewUser(event.target.checked);
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(newUser){
+                //redirect new user here
+                window.location.href = '';
+                }else if(notNewUser){
+                // Redirect registered user here
+                window.location.href = '/stocks';
+                }else{
+                alert('You must select an option to continue.')
+                }
+   };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <label>
+             <h1>Is this your first time logging onto this website?</h1>
+             This is my first time logging onto this website: 
+             <input type="checkbox" name="newUser" checked={newUser} onChange={handleNewUserChange}></input>
+            </label><br/><br/>
+            <label>
+             This is NOT my first time logging onto this website:
+             <input type="checkbox" name="notNewUser" checked={notNewUser} onChange={handleNotNewUserChange}></input>   
+            </label><br/><br/>
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
+
+export default Form;
