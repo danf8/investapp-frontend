20e83aba862d3377c036d7974ee1c2c4f3c062c7
Kathy <kath.scholle@gmail.com>
2023-03-21 16:15:36 -0400

    fixing

--- src/pages/UserStockData.js
+++ src/pages/UserStockData.js
@@ -2,26 +2,27 @@
 import '../Css/Userstock.css';
 
 const UserStockData = (props) => {
-  const updateNewUser = async(ownedStocks, currentMoney, uid) => {
-    try {
-      if (props.user) {
-        const token = await props.user.getIdToken();
-        await fetch('http://localhost:3002/user', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'Application/json',
-            'Authorization': 'Bearer ' + token
-          },
-          body: JSON.stringify({ownedStocks, currentMoney, uid}) // empty object for now
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+
+  // const updateNewUser = async(ownedStocks, currentMoney, uid) => {
+  //   try {
+  //     if (props.user) {
+  //       const token = await props.user.getIdToken();
+  //       await fetch('http://localhost:3002/user', {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-Type': 'Application/json',
+  //           'Authorization': 'Bearer ' + token
+  //         },
+  //         body: JSON.stringify({ownedStocks, currentMoney, uid}) // empty object for now
+  //       })
+  //       .catch((error) => {
+  //         console.error(error);
+  //       });
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
   console.log(props.userStocks)
   let totalInvestmentValues = 0;
