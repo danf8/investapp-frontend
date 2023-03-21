a939d1e1447b47e12f64a74526b893f96ae7f458
Kathy <kath.scholle@gmail.com>
2023-03-20 21:18:04 -0400

    netlify deployment

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,8 +14,6 @@ const Main = (props) => {
     // const API_URL = "http://localhost:5000/stocks";
     const API_URL = "https://investing-buddy/herokuapp.com/stocks";
 
-    // console.log("New state: " + JSON.stringify(userIndexState))
-
     const getStocks = useCallback(async () => {
         try {
             if (props.user) {
