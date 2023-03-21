00b94b714693790b734a938ab55340ec269ad67f
Kathy <kath.scholle@gmail.com>
2023-03-20 23:08:05 -0400

    update api link

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,37 +1,40 @@
 // import { useState } from 'react';
 import '../Css/Form.css'
+import { useNavigate } from 'react-router-dom';
 
+  const Form = (props) => {
+  const navigate = useNavigate();
+  
+    // const [newUser, setNewUser] = useState(false);
+    // const [notNewUser, setNotNewUser] = useState(false);
 
-const Form = (props) => {
-  // const [newUser, setNewUser] = useState(false);
-  // const [notNewUser, setNotNewUser] = useState(false);
-
-  // const handleNewUserChange = (event) => {
-  //   setNewUser(event.target.checked);
-  // };
-
-  // const handleNotNewUserChange = (event) => {
-  //   setNotNewUser(event.target.checked);
-  // };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    // if(newUser) {
-      //redirect new user here
+    // const handleNewUserChange = (event) => {
+      //   setNewUser(event.target.checked);
+      // };
+      
+      // const handleNotNewUserChange = (event) => {
+        //   setNotNewUser(event.target.checked);
+        // };
+        
+        const handleSubmit = async (event) => {
+          event.preventDefault();
+          // if(newUser) {
+            //redirect new user here
       await updateNewUser([], 100000, props.user.uid);
-      window.location.href = '/stocks';
-    // } else if(notNewUser) {
-      // Redirect registered user here
       // window.location.href = '/stocks';
-    // } else {
-    //   alert('You must select an option to continue.')
-    // }
-  };
+      navigate('/stocks', {replace: true});
+      // } else if(notNewUser) {
+        // Redirect registered user here
+        // window.location.href = '/stocks';
+        // } else {
+          //   alert('You must select an option to continue.')
+          // }
+        };
 
-  const updateNewUser = async(ownedStocks, currentMoney, uid) => {
-    try {
-      if (props.user) {
-        const token = await props.user.getIdToken();
+        const updateNewUser = async(ownedStocks, currentMoney, uid) => {
+          try {
+            if (props.user) {
+              const token = await props.user.getIdToken();
         await fetch('http://localhost:3002/users', {
           method: 'POST',
           headers: {
@@ -48,7 +51,8 @@ const Form = (props) => {
       console.error(error);
     }
   }
-
+  
+}
   return (
     <form id='Form' onSubmit={handleSubmit}>
       {/* <label>
