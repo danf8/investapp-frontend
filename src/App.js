fffd7c7702a54acef5bca3134e03b324d31ee13c
Kathy <kath.scholle@gmail.com>
2023-03-25 18:14:30 -0400

    try redirecting google users

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import {useState, useEffect} from 'react';
 import Main from './components/Main';
 import Nav from './components/Nav';
 import './App.css';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState(null);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(()=>{
     const unsubscribe = auth.onAuthStateChanged(user=>{
-      setUser(user);
-      // if(user){
-      //   // navigate('/form', {replace: true});
-      //   window.location.href = '/form';
-      // }
+      if(user){
+        setUser(user);
+        navigate('/form', {replace: true});
+      }else{
+        setUser(null);
+      }
     });
     return()=>{
       unsubscribe();
     }
-  });
+  }, [navigate]);
 
   return (
     <div className="App">
