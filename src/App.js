b41d44bbbfac25cfd3c27611b717bf1b96287bd9
Kathy <kath.scholle@gmail.com>
2023-03-25 18:23:06 -0400

    debug

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import {useState, useEffect} from 'react';
 import Main from './components/Main';
 import Nav from './components/Nav';
 import './App.css';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState(null);
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   useEffect(()=>{
     const unsubscribe = auth.onAuthStateChanged(user=>{
-      if(user){
+      // if(user){
         setUser(user);
-        navigate('/form', {replace: true});
-      }else{
-        setUser(null);
-      }
+        // navigate('/form', {replace: true});
+      // }else{
+      //   setUser(null);
+      // }
     });
     return()=>{
       unsubscribe();
     }
-  }, [navigate]);
+  });
+  // }, [navigate]);
 
   return (
     <div className="App">
