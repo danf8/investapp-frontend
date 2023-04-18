a8aac4791a8f222a59c364680307a7f66bded63f
Daniel Fields <dgfields4193@gmail.com>
2023-04-18 19:55:34 -0400

    wip: update user form signup

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,72 +1,57 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import '../Css/Form.css'
 import { useNavigate } from 'react-router-dom';
 
   const Form = (props) => {
   const navigate = useNavigate();
-  
-    // const [newUser, setNewUser] = useState(false);
-    // const [notNewUser, setNotNewUser] = useState(false);
 
-    // const handleNewUserChange = (event) => {
-      //   setNewUser(event.target.checked);
-      // };
-      
-      // const handleNotNewUserChange = (event) => {
-        //   setNotNewUser(event.target.checked);
-        // };
+  const [startValue, setStartValue] = useState({
+    ownedStocks: [],
+    currentMoney: 0,
+  })
+
+  const handleFormChange = (event) => {
+    setStartValue((prevState) => ({
+      ...prevState,
+       ownedStocks: [],
+      [event.target.name]: parseInt([event.target.value]),
+    }));
+  };
         
-        const handleSubmit = async (event) => {
-          event.preventDefault();
-          // if(newUser) {
-            //redirect new user here
-      await updateNewUser([], 100000, props.user.uid);
-      // window.location.href = '/stocks';
-      navigate('/stocks', {replace: true});
-      // } else if(notNewUser) {
-        // Redirect registered user here
-        // window.location.href = '/stocks';
-        // } else {
-          //   alert('You must select an option to continue.')
-          // }
-        };
-
-        const updateNewUser = async(ownedStocks, currentMoney, uid) => {
-          try {
-            if (props.user) {
-              const token = await props.user.getIdToken();
-        await fetch(props.API_URL+  'users', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'Application/json',
-            'Authorization': 'Bearer ' + token
-          },
-          body: JSON.stringify({ownedStocks, currentMoney: 10000, uid}) // empty object for now
-        })
-        .catch((error) => {
-          console.error(error);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    updateNewUser(startValue);
+    navigate('/stocks', {replace: true});
+  };
+  
+  // used to set users current money on db
+  const updateNewUser = async(currentMoney) => {
+    try {
+      if (props.user) {
+        const token = await props.user.getIdToken();
+        await fetch(props.API_URL +  'users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'Application/json',
+          'Authorization': 'Bearer ' + token
+        },
+        body: JSON.stringify(currentMoney) 
         });
       }
-    } catch (error) {
+    } catch(error) {
       console.error(error);
     }
-  }
-  
+  };
+
   return (
     <form id='Form' onSubmit={handleSubmit}>
-      {/* <label>
-        <h1>Is this your first time logging onto this website?</h1>
-        This is my first time logging onto this website:
-        <input type="checkbox" name="newUser" checked={newUser} onChange={handleNewUserChange}></input>
-      </label><br/><br/>
-      <label>
-        This is NOT my first time logging onto this website:
-        <input type="checkbox" name="notNewUser" checked={notNewUser} onChange={handleNotNewUserChange}></input>
-      </label><br/><br/> */}
-      {/* <h1>Click here to continue</h1> */}
+      <label>Please enter your starting amount of money: $<input type="number" name="currentMoney" value={startValue.currentMoney} onChange={handleFormChange} id="" /></label>
       <button id='Formbtn' type="submit">Start Learning Now!</button>
     </form>
   );
-}
+};
 
 export default Form;
+
+
+
