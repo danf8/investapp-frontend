639fb5da43f1da2fb0a3ae52b2589d46d7ad5141
Daniel Fields <dgfields4193@gmail.com>
2023-04-19 15:49:48 -0400

    wip: display additional user information

--- src/pages/UserStockData.js
+++ src/pages/UserStockData.js
@@ -4,25 +4,23 @@ const UserStockData = (props) => {
 
 if(props.userStocks !== null) {
   let totalInvestmentValues = 0;
-  props.userStocks.ownedStocks.map((stock, i) =>  { totalInvestmentValues += (stock[0].stockPurchased.price * stock[0].ownedShares);
-     return{ totalInvestmentValues}
-     });
   return (
     <div className='userstockM'>
     <h1>Current Funds in Wallet: ${props.userStocks.currentMoney}</h1>
     <h3>Total Value of Investments: ${totalInvestmentValues}</h3>
     <ul>
       {props.userStocks.ownedStocks.map((stock, i) => {
-        const numberOfShares = +(stock[0].ownedShares)
-        totalInvestmentValues += (stock[0].stockPurchased.price * numberOfShares)
+        let currentPrice = props.stocks.filter((s) => s.symbol === props.userStocks.ownedStocks[i][0].stockPurchased.symbol);
+        totalInvestmentValues += stock[0].stockPurchased.price * stock[0].ownedShares;
         return(
-        <section className='userstock' key={i}>
+          <section className='userstock' key={i}>
         <li><strong>Ticker Symbol: </strong> {stock[0].stockPurchased.symbol}</li>
-        <li><strong>Current Price Per Share: $</strong> {stock[0].stockPurchased.price}</li>
+        <li><strong>Purchased Price: $</strong> {stock[0].stockPurchased.price}</li>
+        <li><strong>Current Price: $</strong> {currentPrice[0].price}</li>
         <li><strong>Number of shares you own: </strong>{stock[0].ownedShares}</li>
-        <li><strong>Value of current owned shares: $</strong> {stock[0].stockPurchased.price * numberOfShares}</li>
+        <li><strong>Value of current owned shares: $</strong> {stock[0].stockPurchased.price * stock[0].ownedShares}</li>
         </section>
-      )})}
+        )})}
     </ul>
     </div>
   )
